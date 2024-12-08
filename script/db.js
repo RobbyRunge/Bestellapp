15cbf4a34e21e39127955c66a979071ce70168aa
RobbyR98 <robbyrunge@yahoo.com>
2024-12-08 18:24:41 +0100

    first upload

---
+++ script/db.js
@@ -0,0 +1,96 @@
+let myDishes = [
+  {
+    "name": "Big Hamburger",
+    "price": 6.50,
+    "description": "Ist ein großer, saftiger Burger, mit einem großzügigen Rindfleischpatty, knackigem Salat, Tomaten, Zwiebeln, Käse, Gurken und einer würzigen Sauce. Wird in einem fluffigen Brötchen serviert. Für alle die es klassisch lieben!"
+  },
+
+  {
+    "name": "Big Cheeseburger",
+    "price": 7.50,
+    "description": "Ist ein herzhafter Klassiker der Fast-Food-Küche: saftiges Rindfleisch-Patty, geschmolzener Käse, knackiger Salat, Tomaten, Zwiebeln, Gurkenscheiben und eine pikante Sauce, serviert in einem weichen, goldbraunen Brötchen. Perfekt für Burger-Liebhaber!"
+  },
+
+  {
+    "name": "Big Chilli Burger",
+    "price": 7.50,
+    "description": "Ist ein würziger Burger mit saftigem Rindfleisch-Patty, scharfem Chili-Topping, cremiger Jalapeño-Sauce, knackigem Salat und geschmolzenem Käse, serviert in einem getoasteten Bun. Perfekt für Liebhaber von feurigem Geschmack! "
+  },
+
+  {
+    "name": "Big Chickenburger",
+    "price": 7.50,
+    "description": "Ist ein großzügiger Burger mit einem saftigen, panierten oder gegrillten Hähnchen-Patty, serviert in einem weichen Brötchen. Enthält Salat, Tomaten, Käse, Gurken, und eine spezielle Sauce, um den Geschmack abzurunden. Ideal für alle, die Hähnchen lieben und einen herzhaften Genuss suchen!"
+  },
+
+  {
+    "name": "Nudeln mit Tomatensauce",
+    "price": 8.50,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Nudeln mit Hackfleischsauce",
+    "price": 9.00,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Spaghetti Carbonara",
+    "price": 11.00,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Spaghetti Bolognese",
+    "price": 11.00,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Broccoli-Auflauf",
+    "price": 11.00,
+    "description": "Nudeln mit würziger Sahnesauce und Brokkolie, mit Käse überbacken."
+  },
+
+  {
+    "name": "Nudel-Auflauf",
+    "price": 11.00,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Gemüse-Auflauf",
+    "price": 11.00,
+    "description": "Ein großer, saftiger Burger, der aus einem dicken Rindfleischpatty, frischem Salat, Tomaten, Gurken, Käse und Zwiebeln besteht, eingerahmt von einem weichen Brötchen. Mit Saucen Ketchup, Mayo und einer speziellen Burger-Soße verfeinert."
+  },
+
+  {
+    "name": "Überbackene Nudeln mit Tomatensauce",
+    "price": 11.00,
+    "description": "Nudeln in würziger Tomatensauce, mit Käse überbacken. Perfekt cremig und herzhaft!"
+  },
+
+  {
+    "name": "Tiramisu",
+    "price": 9.50,
+    "description": "Ist ein klassisches italienisches Dessert aus Löffelbiskuits, Mascarponecreme, Kaffee und Kakaopulver, mit Amaretto verfeinert."
+  },
+
+  {
+    "name": "Schoko Lavakuchen",
+    "price": 8.50,
+    "description": "Ein kleiner, weicher Kuchen mit einer flüssigen Schokoladenfüllung im Inneren, der beim Anschneiden herausfließt. Perfekt für Schokofans!"
+  },
+
+  {
+    "name": "Creme Brule",
+    "price": 8.50,
+    "description": "Ein französisches Dessert aus einer vanillearomatisierten Eier-Sahne-Creme, die mit einer knusprigen Karamellschicht überzogen wird."
+  },
+  {
+    "name": "Schokobrötchen (5 Stück)",
+    "price": 8.50,
+    "description": "gefüllt mit Kinderschokolade, dazu Vanille-Dip"
+  },
+]
