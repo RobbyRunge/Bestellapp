8c1ba3f09bfb53e7e5c138445ebc9854b9be7b1a
RobbyR98 <robbyrunge@yahoo.com>
2024-12-18 18:57:23 +0100

    add cart by buy, calculate for total price and quantity, existingDish, add or remove basket quantitiy with buttons, design new cart and some other things

--- script/script.js
+++ script/script.js
@@ -1,10 +1,25 @@
+let totalPrice = 0;
+let deliveryCosts = 2.00;
+
 function init() {
   renderDishes();
   renderBasket();
 }
 
-let totalPrice = 0;
-let deliveryCosts = 2.00; 
+function openCart() {
+  document.getElementById('overlay').style.display = "block";
+
+  resetCart();
+}
+
+function offCart() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function resetCart() {
+  basket = [];
+  renderBasket();
+}
 
 function formatPrice(price) {
   return new Intl.NumberFormat('de-DE', {
@@ -28,15 +43,40 @@ function renderDishes() {
 function renderBasket() {
   let basketRef = document.getElementById('basket-container-render')
   basketRef.innerHTML = "";
+  let totalPrice = 0;
+
   for (let i = 0; i < basket.length; i++) {
     basketRef.innerHTML += getTemplateBasket(i); 
+    totalPrice += basket[i].price * basket[i].quantity;
   }
+
+  if (basket.length > 0) {
+    basketRef.innerHTML += getTotalPriceTemplate(totalPrice, deliveryCosts);;
+  } else {
+    basketRef.innerHTML = "<p class='basket-empty-text'>Ihr Warenkorb ist leer.</p>";
+  }
+  
 }
 
 function addDishesToBasket(i) {
-  if (myDishes[i]) {
-    basket.push(myDishes[i]);
-  } 
+  let dish = myDishes[i];
+  let existingDish = basket.find(item => item.name === dish.name);
+
+  if (existingDish) {
+    existingDish.quantity++;
+  } else {
+    basket.push({ ...dish, quantity: 1 });
+  }
+
+  renderBasket();
+}
+
+function removeDishes(i) {
+  if (basket[i].quantity > 1) {
+    basket[i].quantity--; 
+  } else {
+    basket.splice(i, 1);
+  }
 
   renderBasket();
 }
--- script/template.js
+++ script/template.js
@@ -31,17 +31,30 @@ function getTemplateBasket(i) {
   return `
     <div class="name-price-amount-position">
       <div class="number-buttons-position">
-        <p><strong>${basket[i].name}</strong></p>
+        <p>${basket[i].name}</p>
         <img onclick="deleteDishes(${i})" class="trash" src="./assets/img/trash.png" alt="" />
       </div>
-      <p><strong>Preis: ${formatPrice(basket[i].price)}</strong></p>
+      <p>Einzelpreis: <strong>${formatPrice(basket[i].price)}</strong></p>
+      <p>Total: <strong>${formatPrice(basket[i].price * basket[i].quantity)}</strong></p>
       <div class="number-buttons-position">
-        <p><strong>Anzahl:</strong></p>
-        <div>
-          <img onclick="addDishes(${basket[i]})" class="plus-minus" src="./assets/img/plus.png" alt="" />
-          <img onclick="removeDishes(${basket[i]})" class="plus-minus" src="./assets/img/minus.png" alt="" />
+        <p>Anzahl:</p>
+        <div class="remove-quantity-add-position">
+          <img onclick="removeDishes(${i})" class="plus-minus" src="./assets/img/minus.png" alt="" />
+          <span><strong>${basket[i].quantity}</strong></span>
+          <img onclick="addDishesToBasket(${i})" class="plus-minus" src="./assets/img/plus.png" alt="" />
         </div>
       </div>
     </div>
-    <div class="border-basket"></div>`
+    <div class="border-basket"></div>`;
+}
+
+function getTotalPriceTemplate(totalPrice, deliveryCosts) {
+  return `
+    <div class="total-price-container">
+      <p>Lieferkosten:<br> <strong>${formatPrice(deliveryCosts)}</strong></p><br>
+      <p>Gesamtpreis (inkl. Lieferkosten):<br> <strong>${formatPrice(totalPrice + deliveryCosts)}</strong></p>
+    </div><br>
+    <div class="basket-buy-container">
+      <button onclick="openCart()" class="basket-buy-position">Zur Kasse <img src="./assets/img/shopping-cart.png" alt="" /></button>
+    </div>`;
 }
